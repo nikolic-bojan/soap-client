8e1d9ac4a1566b44b9fe0c76192a9a357aec5071
Bojan Nikolić <7010347+nikolic-bojan@users.noreply.github.com>
2022-08-17 16:19:18 +0200

    update to net6

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -87,7 +87,6 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHttpsRedirection();
             app.UseHeaderPropagation();
             app.UseForwardedHeaders();
             app.UseStatusCodePages();
